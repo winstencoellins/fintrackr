67c714cc00d60c26ebe31fa5d8589cf8bed6f660
WINSTEN COELLINS <winstencoellins13@gmail.com>
2025-10-24 17:04:29 +0700

    initial commit

---
+++ app/dashboard/page.tsx
@@ -0,0 +1,12 @@
+import { ChartAreaDefault } from "@/components/chart-area";
+import { SectionCards } from "@/components/section-cards";
+
+export default function Dashboard() {
+  return (
+    <div>
+      <SectionCards />
+
+      <ChartAreaDefault />
+    </div>
+  )
+}
---
+++ components/chart-area.tsx
@@ -0,0 +1,90 @@
+"use client"
+
+import { Area, AreaChart, CartesianGrid, XAxis } from "recharts"
+
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card"
+import {
+  ChartConfig,
+  ChartContainer,
+  ChartTooltip,
+  ChartTooltipContent,
+} from "@/components/ui/chart"
+
+export const description = "A simple area chart"
+
+const chartData = [
+  { month: "January", desktop: 186 },
+  { month: "February", desktop: 305 },
+  { month: "March", desktop: 237 },
+  { month: "April", desktop: 73 },
+  { month: "May", desktop: 209 },
+  { month: "June", desktop: 214 },
+  { month: "June", desktop: 214 },
+  { month: "June", desktop: 214 },
+  { month: "June", desktop: 214 },
+  { month: "June", desktop: 214 },
+  { month: "June", desktop: 214 },
+  { month: "June", desktop: 214 },
+  { month: "June", desktop: 214 },
+  { month: "June", desktop: 214 },
+]
+
+const chartConfig = {
+  desktop: {
+    label: "Desktop",
+    color: "var(--chart-1)",
+  },
+} satisfies ChartConfig
+
+export function ChartAreaDefault() {
+  return (
+    <div className="my-8 lg:px-6">
+        <Card className="@container/card">
+            <CardHeader>
+                <CardTitle>Area Chart</CardTitle>
+                <CardDescription>
+                Showing total visitors for the last 6 months
+                </CardDescription>
+            </CardHeader>
+            <CardContent className="h-full">
+                <ChartContainer config={chartConfig} className="aspect-auto h-[250px] w-full">
+                    <AreaChart
+                        accessibilityLayer
+                        data={chartData}
+                        margin={{
+                            left: 12,
+                            right: 12,
+                        }}
+                    >
+                        <CartesianGrid vertical={false} />
+                        <XAxis
+                            dataKey="month"
+                            tickLine={false}
+                            axisLine={false}
+                            tickMargin={8}
+                            tickFormatter={(value) => value.slice(0, 3)}
+                        />
+                        <ChartTooltip
+                            cursor={false}
+                            content={<ChartTooltipContent indicator="line" />}
+                        />
+                        <Area
+                            dataKey="desktop"
+                            type="natural"
+                            fill="var(--color-desktop)"
+                            fillOpacity={0.4}
+                            stroke="var(--color-desktop)"
+                        />
+                    </AreaChart>
+                </ChartContainer>
+            </CardContent>
+        </Card>
+    </div>
+  )
+}
